cde1dab2107a85d769efde4f91a679c3dfbce28f
jyeary <johnyeary@gmail.com>
2020-07-16 17:44:40 -0400

    Added functions

---
+++ functions.ps1
@@ -0,0 +1,21 @@
+# Basic commands
+function which($name) { Get-Command $name -ErrorAction SilentlyContinue | Select-Object Definition }
+function touch($file) { "" | Out-File $file -Encoding ASCII }
+
+### Environment functions
+
+function RefreshEnvironment {
+    $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
+                 'HKCU:\Environment'
+
+    $locations | ForEach-Object {
+        $k = Get-Item $_
+        $k.GetValueNames() | ForEach-Object {
+            $name  = $_
+            $value = $k.GetValue($_)
+            Set-Item -Path Env:\$name -Value $value
+        }
+    }
+
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
